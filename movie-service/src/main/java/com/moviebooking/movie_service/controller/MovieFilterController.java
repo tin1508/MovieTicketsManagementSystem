47c0f415691ec458d795d42999620152d7f1df89
TruongVoKy2k5 <chuhonganh0701@gmail.com>
2025-10-22 21:18:25 +0700

    Hoàn thiện specification.

--- movie-service/src/main/java/com/moviebooking/movie_service/controller/MovieFilterController.java
+++ movie-service/src/main/java/com/moviebooking/movie_service/controller/MovieFilterController.java
@@ -2,6 +2,7 @@ package com.moviebooking.movie_service.controller;
 
 import com.moviebooking.movie_service.dto.request.MovieFilterRequest;
 import com.moviebooking.movie_service.dto.response.MovieResponse;
+import com.moviebooking.movie_service.enums.MovieStatus;
 import com.moviebooking.movie_service.service.MovieService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +22,7 @@ public class MovieFilterController {
     MovieService movieService;
 
     @GetMapping("/filter")
-    public ResponseEntity<Page<MovieResponse>> filterMovie(
+    public ResponseEntity<Page<MovieResponse>> filterMovies(
             @ModelAttribute MovieFilterRequest movieFilterRequest,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
@@ -39,5 +40,59 @@ public class MovieFilterController {
         return ResponseEntity.ok(result);
     }
 
+    @GetMapping("/search")
+    public ResponseEntity<Page<MovieResponse>> searchMovies(
+            @RequestParam String keyword,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+            ) {
+        MovieFilterRequest movieFilterRequest = new MovieFilterRequest();
+        movieFilterRequest.setKeyword(keyword);
 
+        Pageable pageable = PageRequest.of(page, size);
+
+        return ResponseEntity.ok(
+                movieService.searchAndFilterMovies(movieFilterRequest, pageable));
+    }
+
+    @GetMapping("/now-showing")
+    public ResponseEntity<Page<MovieResponse>> getNowShowingMovies(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+            ) {
+        MovieFilterRequest movieFilterRequest = new MovieFilterRequest();
+        movieFilterRequest.setMovieStatus(MovieStatus.NOW_SHOWING);
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by("releaseData").ascending());
+
+        return ResponseEntity.ok(
+                movieService.searchAndFilterMovies(movieFilterRequest, pageable));
+    }
+
+    @GetMapping("/by-genre/{genreId}")
+    public ResponseEntity<Page<MovieResponse>> getMoviesByGenre(
+            @PathVariable Long genreId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        MovieFilterRequest movieFilterRequest = new MovieFilterRequest();
+        movieFilterRequest.setGenreId(genreId);
+
+        Pageable pageable = PageRequest.of(page, size);
+
+        return ResponseEntity.ok(movieService.searchAndFilterMovies(movieFilterRequest, pageable));
+    }
+
+    @GetMapping("/top-rated")
+    public  ResponseEntity<Page<MovieResponse>> getTopRatedMovies(
+            @RequestParam(defaultValue = "10") int limit
+    ) {
+        Pageable pageable = PageRequest.of(0, limit, Sort.by("rating").descending());
+
+        MovieFilterRequest movieFilterRequest = new MovieFilterRequest();
+
+        movieFilterRequest.setMinRating(0.0);
+
+        return  ResponseEntity.ok(movieService.searchAndFilterMovies(movieFilterRequest, pageable));
+    }
 }
